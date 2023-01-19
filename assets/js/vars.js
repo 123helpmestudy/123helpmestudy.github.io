ab708eb4844e70e9f8ce9ae58d655e839ab572c7
Marcel <123helpmestudy@gmail.com>
2023-01-19 13:57:26 +0000

    Added direct dest for url prod

--- assets/js/vars.js
+++ assets/js/vars.js
@@ -4,7 +4,7 @@ const devEnv = true;
 
 // Set all the static variables in the project
 const vars = {
-  baseUrl: devEnv ? 'http://localhost:8000' : 'https://api.123helpmestudy.com',
+  baseUrl: devEnv ? 'http://localhost:8000' : 'https://35.225.123.125',
   stripeUrl: devEnv ? 'pk_test_k2rd0kRfCXUc2I62qwxzuH5A00mUkCi9Hv' : 'pk_live_9mCCKga6w9sl8XyZFOEKkaXZ00T0bptR46',
 }
 
